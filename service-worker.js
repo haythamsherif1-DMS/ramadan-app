408c4e11b647226a8159620e60053dcd40a1cd39
haythamsherif1-DMS <haythamsherif1@gmail.com>
2026-02-22 15:49:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,21 @@
-self.addEventListener("install", e=>{
-e.waitUntil(
-caches.open("ramadan-v2").then(cache=>{
-return cache.addAll([
-"/",
-"index.html",
-"manifest.json",
-"adhan.mp3",
-"icon.png"
-])
-})
-)
-})
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("ramadan-app-v2").then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "manifest.json",
+        "adhan.mp3",
+        "icon.png"
+      ]);
+    })
+  );
+});
 
-self.addEventListener("fetch", e=>{
-e.respondWith(
-caches.match(e.request).then(res=>{
-return res || fetch(e.request)
-})
-)
-})
-
-self.addEventListener("notificationclick", function(event) {
-event.notification.close();
-event.waitUntil(clients.openWindow("/"));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
